f10ce588722e1637d94783e5cb8e9afa3588ed3c
cinemassacres <cinemassacres@gmail.com>
2021-01-02 11:36:59 -0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -191,7 +191,7 @@
     if (!errorDiv) return;
  if (errors.osUnsupported) {
      errorDiv.innerHTML =
-       "The answer <br>to life, the universe, and everything...<br> is 42."   
+       "Make sure Chatterino version 2.1.1 or later is installed and running on your computer <br> Chatterino 1 is not supported. <br> There might be issues with other twitch related extensions."   
    "Make sure Chatterino version 2.1.1 or later is installed and running on your computer."
        
        "Chatterino 1 is not supported."
